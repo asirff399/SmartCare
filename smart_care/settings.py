94222f509b6d2c6cf870c7b8ccba473ac66606e8
Asir Intisar <112148444+asirff399@users.noreply.github.com>
2024-08-11 13:06:09 +0600

    Update settings.py

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from decouple import config
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -145,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL")
-EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD") 
+EMAIL_HOST_USER = config("EMAIL")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD") 
